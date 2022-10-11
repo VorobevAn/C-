d17b48d2fa4959cbfa423e68bd28e86f44382e63
Vorodev <cust89@mail.ru>
2022-10-11 22:42:03 +0500

    1,2 task solved

---
+++ task19/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string namber = Console.ReadLine();
+
+char[] num = new char[5];
+for (int i = 0; i < 5; i++)
+    num[i] = namber[i];
+
+if (num[0] == num[4] && num[1] == num[3])
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+// Console.WriteLine(num[0] );
+// Console.WriteLine(num[4] );
+
+
---
+++ task21/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Введите Ax: ");
+int Ax = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Ay: ");
+int Ay = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Az: ");
+int Az = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Bx: ");
+int Bx = int.Parse(Console.ReadLine());
+
+Console.Write("Введите By: ");
+int By = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Bz: ");
+int Bz = int.Parse(Console.ReadLine());
+
+double rezult = Math.Sqrt(Math.Pow((Bx-Ax), 2) + Math.Pow((By-Ay), 2) + Math.Pow((Bz-Az), 2));
+
+Console.WriteLine(Math.Round(rezult,2));
